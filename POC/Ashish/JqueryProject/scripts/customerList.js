1318891b2e161e2ce049e5dd067bd0b501bf5fe9
santoshkale3 <santosh.kale@globant.com>
2016-10-18 12:32:19 +0530

    POC code added

---
+++ POC/Ashish/JqueryProject/scripts/customerList.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    
+    getCustomersAjax().then(function (data) {
+        $('#CustData').dataTable();
+    });
+
+    
+    //function deleteCustomer(customerId) {
+    //    deleteCustomerAjax().then(function (data) { });
+    //}
+})
+
+function loadEdit(data) {
+    //$(document).load("Customer.html?customerId=" + customerId);
+    window.location = 'Customer.html?customerId=' + data;
+}
+
+function deleteCustomer(customerId) {
+    deleteCustomerAjax(customerId).then(function (data) { });
+}
+
---
+++ POC/Ashish/JqueryProject/scripts/customerServices.js
@@ -0,0 +1,73 @@
+﻿var getCustomersAjax = function () {
+
+    var d = $.Deferred();
+    $.ajax({
+        url: 'http://localhost:52376',
+        type: 'GET',
+        dataType: "jsonp",
+        jsonpCallback: 'callback',
+        useDefaultXhrHeader: false,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (data) {
+        d.resolve(data);
+        $.each(data, function (k, v) {
+            var counter = 1;
+            var custRow = "<tr><td>" + v.Name + "</td>" +
+                            "<td>" + v.DateofBirth + "</td>" +
+                            "<td>" + v.CustomerType + "</td>" +
+                            "<td>" + v.Gender + "</td>" +
+                            "<td>" + v.Hobbies + "</td>" +
+                            "<td><input type=button value='Edit' id='edit" + v.CustomerId + "' onclick=loadEdit(" + v.CustomerId + "); /> <input type=button value='Delete' id='delete" + v.CustomerId + "' onclick=deleteCustomer(" + v.CustomerId + ") /></td></tr>"
+            $('#CustData').append(custRow);
+            counter++;
+        });
+        
+    })
+        .fail(function (data) {
+            alert('failed' + data);
+            d.reject();
+        });
+    return d.promise();
+}
+
+var deleteCustomerAjax = function (customerId) {
+
+    var d = $.Deferred();
+    $.ajax({
+        url: 'http://localhost:52376/Customer/DeleteCustomer/' + customerId,
+        type: 'DELETE',
+        method: 'DELETE',
+        data: customerId,
+        dataType: "jsonp",
+        jsonpCallback: 'callback',
+        useDefaultXhrHeader: false,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (data) {
+        d.resolve(data);
+        var counter = 1;
+        $('#CustData tbody').remove();
+        $.each(data, function (k, v) {
+            if (counter == 1)
+                $('#CustData').append("<tbody>");
+            var custRow = "<tr><td>" + v.Name + "</td>" +
+                            "<td>" + v.DateofBirth + "</td>" +
+                            "<td>" + v.CustomerType + "</td>" +
+                            "<td>" + v.Gender + "</td>" +
+                            "<td>" + v.Hobbies + "</td>" +
+                            "<td><input type=button value='Edit' id='edit" + v.CustomerId + "' onclick=loadEdit(" + v.CustomerId + "); /> <input type=button value='Delete' id='delete" + v.CustomerId + "' onclick=deleteCustomer(" + v.CustomerId + ") /></td></tr>"
+            $('#CustData').append(custRow);
+            counter++;
+        });
+        
+    })
+        .fail(function (data) {
+            alert('failed' + data.text);
+            d.reject();
+        });
+    return d.promise();
+}
+
