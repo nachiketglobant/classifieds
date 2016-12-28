09c12769c95285518982e97fe0b206f5ab56ce7f
prateekjoshi-globant <prateek.joshi@globant.com>
2016-12-28 16:39:02 +0530

    card list component integration

--- Application/Client/src/app/card-list/components/card-list.component.ts
+++ Application/Client/src/app/card-list/components/card-list.component.ts
@@ -1,3 +1,14 @@
-/**
- * Created by prateekjoshi on 28/12/16.
- */
+import { Component } from '@angular/core';
+import { AppState } from '../../app.service';
+
+let styles = require('../styles/card-list.component.scss').toString();
+let tpls = require('../tpls/card-list.component.html').toString();
+
+@Component({
+    selector : 'card-list',
+    styles : [styles],
+    template : tpls
+})
+export class CardListComponent{
+
+}
