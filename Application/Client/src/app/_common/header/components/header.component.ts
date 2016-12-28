a28abcf916800cf8d05efb3032bc42fbd37e4f87
prateekjoshi-globant <prateek.joshi@globant.com>
2016-12-28 16:22:39 +0530

    header component minor fixes

--- Application/Client/src/app/_common/header/components/header.component.ts
+++ Application/Client/src/app/_common/header/components/header.component.ts
@@ -5,7 +5,7 @@ let styles = require('../styles/header.component.scss').toString();
 let tpls = require('../tpls/header.component.html').toString();
 
 @Component({
-  selector:'header'
+  selector:'header',
   styles:[styles],
   template: tpls
 })
